5c35e02be9b562c8341b714bfd28de4f74e6bf81
SouveraineMAB <mabelemoassesouverainetresor@gmail.com>
2026-07-30 06:40:26 +0100

    Correction du chargement repository

--- lib/repositories/task_repository.dart
+++ lib/repositories/task_repository.dart
@@ -1,48 +1,58 @@
+
 import 'package:task_manager/exceptions/task_exception.dart';
 import 'package:task_manager/repositories/repository.dart';
 import 'package:task_manager/models/task.dart';
+import 'package:task_manager/models/urgent_task.dart';
 import 'package:task_manager/services/json_storage.dart';
 
-class TaskRepository implements Repository<Task>{
+class TaskRepository implements Repository<Task> {
   final List<Task> _tasks = [];
   final JsonStorage storage = JsonStorage();
+ 
   @override
-  void add (Task item){
+  void add(Task item) {
+
     _tasks.add(item);
-
     storage.save(
-      _tasks.map((task) =>task.toJson()).toList(),
+      _tasks.map((task) => task.toJson()).toList(),
     );
+
   }
 
   @override
-  List <Task> getAll (){
+  List<Task> getAll() {
+
     return _tasks;
+
   }
 
   @override
-  Task ? findById(int id){
-    for(Task task in _tasks){
-      if(task.id == id){
+  Task findById(int id) {
+    for(Task task in _tasks) {
+      if(task.id == id) {
         return task;
+
       }
+
     }
 
     throw TaskException(
+      "La tâche avec l'id $id n'existe pas",
+    );
 
-    "La tâche avec l'id $id n'existe pas",);
   }
 
   @override
-  void delete(int id){
-    _tasks.removeWhere((task) => task.id == id);
+  void delete(int id) {
+
+    _tasks.removeWhere(
+      (task) => task.id == id,
+    );
 
     storage.save(
       _tasks.map((task) => task.toJson()).toList(),
     );
 
-    storage.save(
-      _tasks.map((task) =>task.toJson()).toList(),
-    );
   }
+
 }
