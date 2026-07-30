ed138ad652fe5eebcfc7e4fc5a4d2acc09173cb1
SouveraineMAB <mabelemoassesouverainetresor@gmail.com>
2026-07-30 05:14:36 +0100

    Amelioration de UrgentTask et gestion des dates

--- lib/models/urgent_task.dart
+++ lib/models/urgent_task.dart
@@ -1,36 +1,47 @@
 import 'package:task_manager/models/priority.dart';
 import 'package:task_manager/models/task.dart';
 
-class UrgentTask extends Task{
+class UrgentTask extends Task {
   String reason;
-
   UrgentTask(
-      int id,
-     String title,
-     Priority priority,
-     DateTime ? dueDate,
-     this.reason
-  ) :super (
-    id,
-    title,
-    priority,
-    dueDate,
-  );
+    int id,
+    String title,
+    Priority priority,
+    DateTime? dueDate,
+    this.reason,
+  ) : super(
+        id,
+        title,
+        priority,
+        dueDate,
+      );
 
   @override
-Map<String, dynamic> toJson() {
-  return {
-    "id": id,
-    "title": title,
-    "priority": priority.name,
-    "status": status.name,
-    "duedate": dueDate?.toIso8601String(),
-    "reason": reason,
-  };
-}
+  Map<String, dynamic> toJson() {
 
-  @override
-  String getDetails (){
-    return "Tache urgente : $title - priorité : $priority - Raiosn :$reason";
+    return {
+      "id": id,
+      "title": title,
+      "priority": priority.name,
+      "status": status.name,
+      "dueDate": dueDate?.toIso8601String(),
+      "reason": reason,
+    };
+
   }
+
+  @override
+  String getDetails() {
+
+    return """
+Tâche urgente :
+Titre : $title
+Priorité : ${priority.name}
+Statut : ${status.name}
+Date limite : ${dueDate ?? "Aucune"}
+Raison : $reason
+""";
+
+  }
+
 }
