1650f96b42b4ed5dc94f238846c684180cdc90ee
SouveraineMAB <mabelemoassesouverainetresor@gmail.com>
2026-07-30 05:10:32 +0100

    correvtion de la classe abstraite

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,27 +1,23 @@
-import 'dart:ffi';
-
 import 'package:task_manager/models/priority.dart';
 import 'package:task_manager/models/task_status.dart';
 
-abstract class Task{
+abstract class Task {
   final int id;
   String title;
   Priority priority;
   TaskStatus status;
   DateTime? dueDate;
-
-  Task (
+  Task(
     this.id,
-     this.title,
-     this.priority,
-     this.dueDate,
+    this.title,
+    this.priority,
+    this.dueDate,
   ) : status = TaskStatus.pending;
- 
- void marquerCommeTerminee (){
-  status =TaskStatus.completed;
- }
 
- String getDetails ();
+  void marquerCommeTerminee() {
+    status = TaskStatus.completed;
+  }
+  String getDetails();
+  Map<String, dynamic> toJson();
 
- Map<String, dynamic > toJson();
 }
--- lib/services/json_storage.dart
+++ lib/services/json_storage.dart
@@ -1,13 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'storage.dart';
 
-
 class JsonStorage implements Storage<Map<String, dynamic>> {
-
   final String fileName = "tasks.json";
-
 
   @override
   Future<void> save(List<Map<String, dynamic>> items) async {
@@ -25,14 +21,12 @@ class JsonStorage implements Storage<Map<String, dynamic>> {
 
     final file = File(fileName);
 
-
     if (!await file.exists()) {
       return [];
     }
 
 
     final content = await file.readAsString();
-
 
     final List<dynamic> data = jsonDecode(content);
 
